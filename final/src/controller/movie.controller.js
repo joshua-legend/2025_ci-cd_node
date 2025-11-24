a4cb1ad506e37142617ea9699015a1a1b90902ca
joshua <iuttn123@gmail.com>
2025-11-24 13:19:35 +0900

    ..

--- final/src/controller/movie.controller.js
+++ final/src/controller/movie.controller.js
@@ -10,7 +10,8 @@ export const getMovies = async (req, res) => {
       },
     },
   });
-  res.json({ movies });
+
+  res.success(movies);
 };
 export const getMovieById = async (req, res) => {
   const { id } = req.params;
@@ -19,16 +20,26 @@ export const getMovieById = async (req, res) => {
       id: +id,
     },
   });
-  res.json({ movie });
+
+  if (!movie) res.notFound();
+
+  res.success(movie);
 };
 
 export const createMovie = async (req, res) => {
   const { name, rating } = req.body;
+  if (!name || !rating) res.validationError(`name 또는 rating 필수로 입력해야 합니다.`);
+  if (name.length <= 0 || name.length > 255) res.validationError(`name의 길이는 1 ~ 255자이어야 합니다.`);
+
+  const movie = await prisma.movies.findFirst({ where: { name: name } });
+  if (movie) res.conflict(`${name}은 존재합니다.`);
+  if (rating < 1 || rating > 5 || isNaN(rating)) res.validationError("rating은 1 ~ 5의 실수로 입력해야 합니다.");
+
   await prisma.movies.create({
     data: {
       name,
       rating,
     },
   });
-  res.json({ msg: `${name}영화가 생성되었습니다.` });
+  res.success(null, `${name} 영화가 생성되었습니다.`);
 };
