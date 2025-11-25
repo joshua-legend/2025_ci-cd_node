70afa9daccac6665fd27c921c2fe2cbd0ed00d1f
joshua <iuttn123@gmail.com>
2025-11-25 11:17:36 +0900

    validator

--- final/src/controller/movie.controller.js
+++ final/src/controller/movie.controller.js
@@ -10,66 +10,47 @@ export const getMovies = async (req, res) => {
       },
     },
   });
-
   res.success(movies);
 };
-export const getMovieById = async (req, res) => {
-  const { id } = req.params || {};
 
-  if (!id || isNaN(id)) res.validationError("id 데이터가 유효하지 않습니다.");
+export const getMovieById = async (req, res) => {
+  const { id } = req.params;
   const movie = await prisma.movies.findUnique({
     where: {
       id: +id,
     },
   });
-
   if (!movie) res.notFound();
   res.success(movie);
 };
 
 export const createMovie = async (req, res) => {
-  const { name, rating } = req.body || {};
-
-  if (!req.body) res.validationError("body가 필요합니다!");
-  if (!name || !rating) res.validationError(`name 또는 rating 필수로 입력해야 합니다.`);
-  if (name.length <= 0 || name.length > 255) res.validationError(`name의 길이는 1 ~ 255자이어야 합니다.`);
+  const { name, rating } = req.body;
 
   const movie = await prisma.movies.findFirst({ where: { name: name } });
   if (movie) res.conflict(`${name}은 존재합니다.`);
-  if (rating < 1 || rating > 5 || isNaN(rating)) res.validationError("rating은 1 ~ 5의 실수로 입력해야 합니다.");
-
   await prisma.movies.create({
     data: {
-      name,
-      rating,
+      name: name,
+      rating: rating,
     },
   });
   res.success(null, `${name} 영화가 생성되었습니다.`);
 };
 
 export const deleteMovie = async (req, res) => {
-  const { id } = req.params || {};
-  if (!id || isNaN(id)) res.validationError("id 데이터가 유효하지 않습니다.");
-
+  const { id } = req.params;
   const target = await prisma.movies.findUnique({ where: { id: +id } });
   if (!target) res.notFound("존재하지 않는 영화입니다.");
-
   await prisma.movies.delete({ where: { id: +id } });
-
   res.success(null, `${target.name} 영화가 삭제 되었습니다.`);
 };
 
 export const updateMovie = async (req, res) => {
-  const { id } = req.params || {};
-  const { name, rating } = req.body || {};
-
-  if (!id || isNaN(id)) res.validationError("id 데이터가 유효하지 않습니다.");
-  if (name && (name.length <= 0 || name.length > 255)) res.validationError(`name의 길이는 1 ~ 255자이어야 합니다.`);
-  if (rating && (rating < 1 || rating > 5 || isNaN(rating))) res.validationError("rating은 1 ~ 5의 실수로 입력해야 합니다.");
-
+  const { id } = req.params;
+  const { name, rating } = req.body;
   const target = await prisma.movies.findUnique({ where: { id: +id } });
   if (!target) res.notFound("해당 영화는 존재하지 않습니다.");
-
   await prisma.movies.update({
     where: { id: +id },
     data: {
@@ -77,5 +58,5 @@ export const updateMovie = async (req, res) => {
       rating: rating || target.rating,
     },
   });
-  res.success(null, `${target.name} 영화가  되었습니다.`);
+  res.success(null, `${target.name} 영화가 되었습니다.`);
 };
--- final/src/routes/movie.routes.js
+++ final/src/routes/movie.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { createMovie, deleteMovie, getMovieById, getMovies, updateMovie } from "../controller/movie.controller.js";
+import { movieParamsValidator, movieBodyValidator, movieParamsAndBodyValidator } from "../middleware/validate.middleware.js";
 
 const router = express.Router();
 
 router.get("/", getMovies);
-router.get("/:id", getMovieById);
-router.post("/register", createMovie);
-router.delete("/:id", deleteMovie);
-router.put("/:id", updateMovie);
+router.get("/:id", movieParamsValidator, getMovieById);
+router.post("/register", movieBodyValidator, createMovie);
+router.delete("/:id", movieParamsValidator, deleteMovie);
+router.put("/:id", movieParamsAndBodyValidator, updateMovie);
 
 export default router;
